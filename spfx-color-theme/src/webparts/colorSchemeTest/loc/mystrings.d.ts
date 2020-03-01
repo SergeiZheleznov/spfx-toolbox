48e0d53b43cbc6112de58e3b09f47a5eb4d1477c
Sergei Zheleznov <sksdes@gmail.com>
2020-03-01 13:36:15 +0100

    Added sample spfx-color-theme

---
+++ spfx-color-theme/src/webparts/colorSchemeTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IColorSchemeTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ColorSchemeTestWebPartStrings' {
+  const strings: IColorSchemeTestWebPartStrings;
+  export = strings;
+}
