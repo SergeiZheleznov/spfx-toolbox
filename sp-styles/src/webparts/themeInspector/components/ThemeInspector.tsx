253aebd7b7db87e4cf197a9f07a320a80e1692e4
Sergei Zheleznov <sksdes@gmail.com>
2020-05-03 13:58:17 +0200

    updated component ThemeInspector

--- sp-styles/src/webparts/themeInspector/components/ThemeInspector.tsx
+++ sp-styles/src/webparts/themeInspector/components/ThemeInspector.tsx
@@ -4,13 +4,19 @@ import { IThemeInspectorProps } from './IThemeInspectorProps';
 import {useContext} from "react";
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 import {ConfigurationContext} from "../../shared/ConfigurationContext";
+import {Text} from '@fluentui/react';
 
 export const ThemeInspector: React.FunctionComponent<IThemeInspectorProps> = (props) => {
   const config = useContext(ConfigurationContext);
   const {semanticColors}: IReadonlyTheme = config.themeVariant;
     return (
       <div className={ styles.themeInspector }>
-        {JSON.stringify(semanticColors)}
+        {Object.keys(semanticColors).map(key => {
+          const color = semanticColors[key];
+          return(<div className={styles.item} style={{background: color}}>
+            <Text variant={'small'}>{key}</Text>
+          </div>);
+        })}
       </div>
     );
 };
