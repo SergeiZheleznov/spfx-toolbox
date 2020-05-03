9e880b7280b7cf179a3b83242f167e511ca75e2a
Sergei Zheleznov <sksdes@gmail.com>
2020-05-03 16:07:16 +0200

    updated styles

--- sp-styles/src/webparts/themeInspector/components/ThemeInspector.tsx
+++ sp-styles/src/webparts/themeInspector/components/ThemeInspector.tsx
@@ -4,19 +4,40 @@ import { IThemeInspectorProps } from './IThemeInspectorProps';
 import {useContext} from "react";
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 import {ConfigurationContext} from "../../shared/ConfigurationContext";
-import {Text} from '@fluentui/react';
+import {Stack, IconButton} from '@fluentui/react';
 
 export const ThemeInspector: React.FunctionComponent<IThemeInspectorProps> = (props) => {
   const config = useContext(ConfigurationContext);
   const {semanticColors}: IReadonlyTheme = config.themeVariant;
     return (
       <div className={ styles.themeInspector }>
-        {Object.keys(semanticColors).map(key => {
-          const color = semanticColors[key];
-          return(<div className={styles.item} style={{background: color}}>
-            <Text variant={'small'}>{key}</Text>
-          </div>);
-        })}
+        <div className={styles.grid}>
+          <div className={styles.row}>
+            {Object.keys(semanticColors).map(key => {
+              const color = semanticColors[key];
+              const style: React.CSSProperties = {
+                background: color,
+                borderColor: semanticColors.inputBorder,
+              };
+              return(
+                <div className={styles.item} >
+                  <div className={styles.body} style={{borderColor: semanticColors.bodyDivider}}>
+                    <Stack horizontal horizontalAlign={'space-between'}>
+                      <Stack.Item>
+                        <div className={styles.color} style={style}></div>
+                        <span className={styles.name}>{key}</span>
+                      </Stack.Item>
+                      <Stack.Item>
+                        <IconButton iconProps={{iconName: 'Paste'}} onClick={()=>{alert('sdf');}} />
+                      </Stack.Item>
+                    </Stack>
+
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
 };
