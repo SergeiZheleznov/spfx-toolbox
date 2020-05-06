d4db402d5c20d21321a45026a976fb8dbd4ea04c
Sergei Zheleznov <sksdes@gmail.com>
2020-05-06 20:51:35 +0200

    added initial solution sp-schema-extensions

---
+++ sp-schema-extensions/src/webparts/extensionsExplorer/components/IExtensionsExplorerProps.ts
@@ -0,0 +1,3 @@
+export interface IExtensionsExplorerProps {
+  description: string;
+}
---
+++ sp-schema-extensions/src/webparts/extensionsExplorer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IExtensionsExplorerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ExtensionsExplorerWebPartStrings' {
+  const strings: IExtensionsExplorerWebPartStrings;
+  export = strings;
+}
