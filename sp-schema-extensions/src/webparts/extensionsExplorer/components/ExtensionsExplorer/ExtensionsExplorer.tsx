2eb7947e34a9e98291ba2de22e5b9969fc3a2c1d
Sergei Zheleznov <sksdes@gmail.com>
2020-06-08 21:49:37 +0200

    added modal window view

--- sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsExplorer/ExtensionsExplorer.tsx
+++ sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsExplorer/ExtensionsExplorer.tsx
@@ -3,10 +3,11 @@ import styles from './ExtensionsExplorer.module.scss';
 import { IExtensionsExplorerProps } from './IExtensionsExplorerProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import {SchemaExtension} from '@microsoft/microsoft-graph-types';
-import {useContext, useEffect, useState} from "react";
-import {PrimaryButton, Spinner} from '@fluentui/react';
+import {useEffect, useState} from "react";
+import {PrimaryButton, Spinner, Modal} from '@fluentui/react';
+import {useBoolean} from '@uifabric/react-hooks';
 import {ExtensionItemViewer, ExtensionsList} from "../";
-import {Logger, LogLevel} from "@pnp/logging";
+import {Logger} from "@pnp/logging";
 
 const LOG_SOURCE: string = 'ExtensionsExplorer';
 
@@ -14,14 +15,18 @@ export const ExtensionsExplorer: React.FunctionComponent<IExtensionsExplorerProp
 
   const [extensionsAvailable, setExtensionsAvailable] = useState<SchemaExtension[]>(new Array<SchemaExtension>());
   const [selectedItem, setSelectedItem] = useState<SchemaExtension>(null);
+  const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
   const {customSchemaService} = props;
 
   useEffect(() => {
-    Logger.write(`[${LOG_SOURCE}] useEffect()`);
     (async () => {
-      const schemas = await customSchemaService.top(15).get();
-      setExtensionsAvailable(schemas);
-      setSelectedItem(schemas[0]);
+      Logger.write(`[${LOG_SOURCE}] retrieving custom schema extensions`);
+      try {
+        const schemas = await customSchemaService.top(15).get();
+        setExtensionsAvailable(schemas);
+      } catch (e) {
+        Logger.error(e);
+      }
     })();
   },[]);
 
@@ -36,19 +41,32 @@ export const ExtensionsExplorer: React.FunctionComponent<IExtensionsExplorerProp
     })();
   };
 
+  const onItemSelected = (schemaExtension: SchemaExtension) => {
+    setSelectedItem(schemaExtension);
+    showModal();
+  };
+
   return(
     <div className={styles.extensionsExplorer}>
 
       {extensionsAvailable && extensionsAvailable.length ?
         <ExtensionsList
           selectedItem={selectedItem}
-          setSelectedItem={setSelectedItem}
+          onItemSelected={onItemSelected}
           items={extensionsAvailable} /> :
         <Spinner label="Seriously, still loading..." ariaLive="assertive" labelPosition="top" />
       }
       <PrimaryButton onClick={nextButtonClickHandler}>Next</PrimaryButton>
 
-      {selectedItem ? <ExtensionItemViewer schemaExtension={selectedItem} /> : 'empty'}
+      <Modal
+        isOpen={isModalOpen}
+        onDismiss={hideModal}
+        isBlocking={false}
+      >
+        {selectedItem ? <div style={{maxWidth: 750, padding: 15, minWidth:450}} className={styles.modal}>
+          <ExtensionItemViewer schemaExtension={selectedItem} />
+        </div> : 'empty'}
+      </Modal>
     </div>
   );
 };
--- sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsListItem/ExtensionsListItem.tsx
+++ sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsListItem/ExtensionsListItem.tsx
@@ -5,40 +5,43 @@ import styles from './ExtensionsListItem.module.scss';
 import {useContext} from "react";
 import {ConfigurationContext, stringToColour} from "../../../../shared";
 import {IReadonlyTheme} from "@microsoft/sp-component-base";
+import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
 
 export const ExtensionsListItem: React.FunctionComponent<IExtensionsListItemProps> = (props) => {
   const config = useContext(ConfigurationContext);
   const {semanticColors}: IReadonlyTheme = config.themeVariant;
   const {schemaExtension, active} = props;
 
+
   return(
     <div className={styles.item}>
-      <div className={styles.body} style={{
-        borderColor: stringToColour(schemaExtension.owner)
-      }}>
-        <div className={styles.info}>
-          <Text className={styles.name} nowrap={true} block={true}>{schemaExtension.id}</Text>
-          <Text className={styles.description} nowrap={true} block={true} style={{
-            color: semanticColors.bodySubtext
+      <Card tokens={{childrenMargin: 12}} data-schema={schemaExtension.id} onClick={props.onItemSelect}>
+        <Card.Item fill>
+          <div onClick={props.onItemSelect} className={styles.body} style={{
+            borderColor: stringToColour(schemaExtension.owner)
           }}>
-            {schemaExtension.description ? schemaExtension.description : '—'}
-          </Text>
-          <div className={styles.extTargetTypes}>
-            {schemaExtension.targetTypes.map(t => (<span style={{
-              backgroundColor: semanticColors.disabledBackground,
-              color: semanticColors.disabledText
-            }}>{t}</span>))}
+            <div className={styles.info}>
+              <Text className={styles.name} nowrap={true} block={true}>{schemaExtension.id}</Text>
+              <Text className={styles.description} nowrap={true} block={true} style={{
+                color: semanticColors.bodySubtext
+              }}>
+                {schemaExtension.description ? schemaExtension.description : '—'}
+              </Text>
+              <div className={styles.extTargetTypes}>
+                {schemaExtension.targetTypes.map(t => (<span style={{
+                  backgroundColor: semanticColors.disabledBackground,
+                  color: semanticColors.disabledText
+                }}>{t}</span>))}
+              </div>
+            </div>
+            <Stack horizontal horizontalAlign={'space-between'}>
+              <Stack.Item>
+                <ActionButton disabled iconProps={{iconName: 'AllApps'}}>{schemaExtension.properties.length}</ActionButton>
+              </Stack.Item>
+            </Stack>
           </div>
-        </div>
-        <Stack horizontal horizontalAlign={'space-between'}>
-          <Stack.Item>
-            <ActionButton disabled iconProps={{iconName: 'AllApps'}}>{schemaExtension.properties.length}</ActionButton>
-          </Stack.Item>
-          <Stack.Item>
-            <ActionButton data-schema={schemaExtension.id} onClick={props.onItemSelect} iconProps={{iconName: active ? 'RadioBtnOn' : 'RedEye'}} />
-          </Stack.Item>
-        </Stack>
-      </div>
+        </Card.Item>
+      </Card>
     </div>
   );
 };
