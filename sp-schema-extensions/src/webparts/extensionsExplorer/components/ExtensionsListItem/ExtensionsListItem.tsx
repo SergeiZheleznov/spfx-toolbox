e9783fac8ef763d40bebcdec8a64c356b4def910
Sergei Zheleznov <sksdes@gmail.com>
2020-06-10 23:09:40 +0200

    updated extension card layout

--- sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsListItem/ExtensionsListItem.tsx
+++ sp-schema-extensions/src/webparts/extensionsExplorer/components/ExtensionsListItem/ExtensionsListItem.tsx
@@ -5,21 +5,20 @@ import styles from './ExtensionsListItem.module.scss';
 import {useContext} from "react";
 import {ConfigurationContext, stringToColour} from "../../../../shared";
 import {IReadonlyTheme} from "@microsoft/sp-component-base";
-import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
+import {Card} from '@uifabric/react-cards';
 
 export const ExtensionsListItem: React.FunctionComponent<IExtensionsListItemProps> = (props) => {
   const config = useContext(ConfigurationContext);
   const {semanticColors}: IReadonlyTheme = config.themeVariant;
   const {schemaExtension, active} = props;
 
-
   return(
     <div className={styles.item}>
-      <Card tokens={{childrenMargin: 12}} data-schema={schemaExtension.id} onClick={props.onItemSelect}>
-        <Card.Item fill>
-          <div onClick={props.onItemSelect} className={styles.body} style={{
-            borderColor: stringToColour(schemaExtension.owner)
-          }}>
+      <div className={styles.body} style={{
+        borderColor: stringToColour(schemaExtension.owner)
+      }}>
+        <Card tokens={{childrenMargin: 12}}>
+          <Card.Item fill>
             <div className={styles.info}>
               <Text className={styles.name} nowrap={true} block={true}>{schemaExtension.id}</Text>
               <Text className={styles.description} nowrap={true} block={true} style={{
@@ -39,9 +38,16 @@ export const ExtensionsListItem: React.FunctionComponent<IExtensionsListItemProp
                 <ActionButton disabled iconProps={{iconName: 'AllApps'}}>{schemaExtension.properties.length}</ActionButton>
               </Stack.Item>
             </Stack>
-          </div>
-        </Card.Item>
-      </Card>
+          </Card.Item>
+          <Card.Section horizontal styles={{root:{
+            borderTop: `1px solid ${semanticColors.bodyFrameDivider}`
+            }}} tokens={{ padding: '12px 0px 0px' }}>
+            <ActionButton data-schema={schemaExtension.id} onClick={props.onItemSelect} iconProps={{
+              iconName: 'RedEye'
+            }} />
+          </Card.Section>
+        </Card>
+      </div>
     </div>
   );
 };
